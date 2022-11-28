203ae3fd107dcfd632d087630f77552bec28215b
SMOKINFRY <jcrooge@gmail.com>
2022-11-28 18:10:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+  
+  <br />
+  <br />
+
+  <h2 align="center">Joakim - Portfolio Website</h2>
+
+A fully responsive personal portfolio website, <br />Responsive for all devices, built using HTML, CSS, and JavaScript.
+
+<a href="#"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Joakim Desktop Demo](./ "Desktop Demo")
+
+### Contact
+
+If you want to contact with me you can reach me at
---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * THEME SELECTOR / DARK MODE
+ */
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'light');
+      document.getElementById('lightmode').classList.remove('hidden');
+      document.getElementById('darkmode').classList.add('hidden');
+  }
+  else {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      document.getElementById('lightmode').classList.add('hidden');
+      document.getElementById('darkmode').classList.remove('hidden');
+  }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
